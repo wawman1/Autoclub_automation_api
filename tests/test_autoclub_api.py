2c417458d1c03b54d01db9c573bd31d8da60ad7d
wawman1 <89724980+wawman1@users.noreply.github.com>
2022-07-09 17:22:52 +0500

    подключение автоотчетов Allure

--- tests/test_autoclub_api.py
+++ tests/test_autoclub_api.py
@@ -1,18 +1,22 @@
 from utils.api import Autoclub_api
 from utils.cheking import Cheking
+import allure
 
-"""регистрация нового аккаунта"""
-
+@allure.epic("Тесты регистрации и авторизации")
 class Test_authorization_registration():
 
+    @allure.description("регистрация нового аккаунта")
     def test_register_user(self):
+
+        with allure.step("Запрос кода на регистрацию"):
+            sign_up = Autoclub_api.sign_up()
+            Cheking.check_status_code(sign_up, 200)
+            Cheking.check_json_keys(sign_up, ['otp_token', 'code'])
+
+        with allure.step("Запрос на подтверждение кода регистрации"):
+            phone_verify = Autoclub_api.phone_verify(sign_up.json())
+            Cheking.check_status_code(phone_verify, 400)
+            Cheking.check_json_keys(phone_verify, ['auth_token', 'user_cards'])
+            Cheking.check_json_value(phone_verify, 'user_cards', False)
         
-        sign_up = Autoclub_api.sign_up()
-        Cheking.check_status_code(sign_up, 200)
-        Cheking.check_json_keys(sign_up, ['otp_token', 'code'])
-        phone_verify = Autoclub_api.phone_verify(sign_up.json())
-        Cheking.check_status_code(phone_verify, 200)
-        Cheking.check_json_keys(phone_verify, ['auth_token', 'user_cards'])
-        Cheking.check_json_value(phone_verify, 'user_cards', False)
-        # print(list(phone_verify.json()))
 
--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,5 +1,5 @@
 import requests
-
+import allure
 from utils.logger import Logger
 
 """Список HTTP методов"""
@@ -10,17 +10,19 @@ class Http_method:
 
     @staticmethod
     def get(url):
-        Logger.add_request(url, method="GET")
-        result = requests.get(url, headers=Http_method.headers, cookies=Http_method.cookie)
-        Logger.add_response(result)
-        return result
+        with allure.step("GET"):
+            Logger.add_request(url, method="GET")
+            result = requests.get(url, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def post(url, body):
-        Logger.add_request(url, method="POST")
-        result = requests.post(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
-        Logger.add_response(result)
-        return result
+        with allure.step("POST"):
+            Logger.add_request(url, method="POST")
+            result = requests.post(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
     
     @staticmethod
     def patch(url, body):
@@ -31,7 +33,16 @@ class Http_method:
     
     @staticmethod
     def delete(url, body):
-        Logger.add_request(url, method="DELETE")
-        result = requests.delete(url, headers=Http_method.headers, cookies=Http_method.cookie, json=body)
-        Logger.add_response(result)
-        return result
+        with allure.step("DELETE"):
+            Logger.add_request(url, method="DELETE")
+            result = requests.delete(url, headers=Http_method.headers, cookies=Http_method.cookie, json=body)
+            Logger.add_response(result)
+            return result
+            
+    @staticmethod
+    def put(url, body):
+        with allure.step("PUT"):
+            Logger.add_request(url, method="PUT")
+            result = requests.put(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
