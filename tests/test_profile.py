d497cf567f41b1eb1e3c4a27b2cde05577ba5f9b
wawman1 <89724980+wawman1@users.noreply.github.com>
2022-08-15 11:18:38 +0500

    привел файл к общему виду и добавил тест test_profile_update

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,14 +1,33 @@
 from utils.api import Autoclub_api
 from utils.cheking import Cheking
 import allure
+from conftest import Secondary_functions
+import pytest
 
 @allure.epic("Тесты профиля")
+@pytest.mark.profile
 class Test_profile():
 
-    with allure.step("Получение данных профиля"):
+    @allure.story("Позитивные тесты профиля")
+    @pytest.mark.positive
+    class Test_positive_profile():
+
         def test_get_profile(self, base_url, auth_token):
+
+            with allure.step("Получение данных профиля"):
                 get_profile = Autoclub_api.Profile_api.profile(base_url, auth_token)
                 Cheking.check_status_code(get_profile, 200)
                 Cheking.check_json_keys(get_profile, ['id', 'phone', 'name', 'email', 'birth_date'])
-                # Cheking.check_json_value(get_profile, 'id', 123)
-    
+        
+        def test_profile_update(self, base_url, auth_token):
+
+                with allure.step("Добавление email в профиль"):
+                    update_profile = Autoclub_api.Profile_api.profile_update(base_url, auth_token, email=Secondary_functions.random_email())
+                    Cheking.check_status_code(update_profile, 200)
+                    Cheking.check_json_keys(update_profile, ['id', 'phone', 'name', 'email', 'birth_date'])
+        
+    # @allure.story("Негативные тесты профиля")
+    # @pytest.mark.negativ
+    # class Test_positive_profile():
+
+        
